ad3a06ebce9a8b13499a2d506c3fcbb2255d1900
dklb <DakotaLeB@gmail.com>
2020-11-27 13:34:28 -0800

    move spacebar from default to right thumb. fix flashing (requires different bootloader for elite-c and promicro)

--- keyboards/crkbd/keymaps/dklb/rules.mk
+++ keyboards/crkbd/keymaps/dklb/rules.mk
@@ -1,1 +1,12 @@
 OLED_DRIVER_ENABLE  = yes
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+#// comment out next line to flash promicro, uncomment for elite-c
+#BOOTLOADER = qmk-dfu
